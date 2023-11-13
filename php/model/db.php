9db42ab26ffc79c9bc1c389998ccc0464a6bec7c
Liamcr <cariou.liam@orange.fr>
2023-11-13 02:39:33 +0100

    db

--- php/model/db.php
+++ php/model/db.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
      private $host = "localhost";
-    /private $db_name = "inventaire";
+    private $db_name = "inventaire";
      private $username = "root";
      private $password = "";
 
