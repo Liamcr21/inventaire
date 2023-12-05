8e423a750b7b3ee7bcdd7589eeb874b4bd429af0
Liamcr <cariou.liam@orange.fr>
2023-12-05 11:44:32 +0100

    debug warning

--- pret/delete.php
+++ pret/delete.php
@@ -1,3 +1,7 @@
+<?php 
+error_reporting(E_ERROR | E_PARSE);
+
+?>
 <?php
 require_once '../php/model/emprunt.php';
 
--- pret/list.php
+++ pret/list.php
@@ -1,4 +1,7 @@
+<?php 
+error_reporting(E_ERROR | E_PARSE);
 
+?>
 
 <!doctype html>
 <html lang="fr">
